a7467ae92e17a0200fb2b65ef328cee468e3644a
Chuck Burchard <clburchard@gmail.com>
2015-01-17 11:38:07 -0500

    First Commit

---
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,47 @@
+﻿using Moq;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver: IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns( new List<Product>
+            {
+                new Product { Name = "Football", Price = 25 },
+                new Product { Name = "Surf Board", Price = 179 },
+                new Product { Name = "Running Shoes", Price = 95 },
+                new Product { Name = "Lifting Gloves", Price = 19 },
+                new Product { Name = "Golf Umbrella", Price = 39 }
+            });
+
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+        }
+    }
+}
